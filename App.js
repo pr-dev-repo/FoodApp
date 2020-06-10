64467a22bacde444e12caa4d118da7d4921a8ebd
Gabriel Rodríguez Rivera <gabriel.a.rodriguez@cgi.com>
2020-06-10 19:34:50 -0400

    Initial commit -Added searchBox with icon

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import SearchScreen from './src/screens/SearchScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const navigator = createStackNavigator({
+  Search: SearchScreen
+
+}, {
+    initialRouteName: 'Search',
+    defaultNavigationOptions: {
+      title: 'Business Search'
+    }
+
+  });
+
+export default createAppContainer(navigator);
